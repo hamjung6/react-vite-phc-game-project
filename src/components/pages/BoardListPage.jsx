2a973b2f81a0c807f358faa1fbf4e1fe9b4b2bcc
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-10-15 10:19:13 +0900

    commit

---
+++ src/components/pages/BoardListPage.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import boardService from "../../services/BoardService";
+
+const BoardListPage = () => {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    console.log("use Effective 실행");
+    initBoards();
+  }, []);
+
+  const initBoards = () => {
+    boardService.getPagingList().then((response) => {
+      console.log(response);
+    });
+  };
+
+  return <div></div>;
+};
+
+export default BoardListPage;
